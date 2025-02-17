540540154b0c8ef10e4d60edbbd7112ad3677e9c
unknown <adrianapaivausa@icloud.com>
2025-02-17 16:00:57 -0500

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+//Wrap around every page inside the folder
+
+import { DM_Sans } from 'next/font/google'
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+import "./globals.css";
+
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-dm-sans',
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={dmSans.variable}>
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
