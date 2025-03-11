cf36c5bdcd67f58425caa68f2d3c579a8ace0f20
Adriana Paiva <adrianapaivausa@icloud.com>
2025-03-11 19:00:11 -0300

    Bento box styling on progress

--- src/slices/Bento/index.tsx
+++ src/slices/Bento/index.tsx
@@ -27,19 +27,28 @@ const Bento: FC<BentoProps> = ({ slice }) => {
             </h2>
           ),
           em: ({ children }) => (
-            <em className="bg-gradient-t-b from-">{children}</em>
+            <em className="from-primary to-accent bg-gradient-to-b bg-clip-text not-italic text-transparent">
+              {children}
+            </em>
           ),
         }}
       />
-      <PrismicRichText field={slice.primary.body} />
+      <div className="text-tertiary mx-auto mt-6 max-w-md text-balance text-center">
+        <PrismicRichText field={slice.primary.body} />
+      </div>
 
-      {slice.primary.items.map((item) => (
-        <div className="glass-container" key={asText(item.title)}>
-          <PrismicRichText field={item.title} />
-          <PrismicRichText field={item.body} />
-          <PrismicNextImage field={item.image} />
-        </div>
-      ))}
+      <div className="mt-16 grid max-w-4xl grid-rows-[auto_auto_auto] gap-8 md:grid-cols-3 md:gap-10">
+        {slice.primary.items.map((item) => (
+          <div
+            className="glass-container to gray-950 row-span-3 grid grid-rows-subgrid gap-4 rounded-lg bg-gradient-to-b from-gray-900 p-4"
+            key={asText(item.title)}
+          >
+            <PrismicRichText field={item.title} />
+            <PrismicRichText field={item.body} />
+            <PrismicNextImage field={item.image} />
+          </div>
+        ))}
+      </div>
     </Bounded>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,13 +4,22 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
+
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-dm-sans)"],
+        sans: ['var(--font-dm-sans)'],
       },
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)',
+        accent: 'var(--color-accent)',
+        background: 'var(--color-background)',
+        foreground: 'var(--color-foreground)',
+      }
     },
   },
   plugins: [],
-};
+}
